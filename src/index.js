a04d66e5b612c8206010bec9ee7fc787097ac67e
Mateo <mateo.orolot@gmail.com>
2023-10-28 16:03:45 -0500

    Render products with handlebars

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const handlebars = require("express-handlebars");
+const path = require("path");
 const productsRouter = require("./routes/products.routes");
 const cartsRouter = require("./routes/carts.routes");
+const ProductMaganer = require("./class/productManager");
+
+const productManager = new ProductMaganer();
 
 const app = express();
 
@@ -8,8 +13,18 @@ const PORT = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "handlebars");
 
 app.use("/api", productsRouter, cartsRouter);
+
+app.get("/", async (req, res) => {
+  const products = await productManager.getProducts();
+  res.render("home", {products});
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
